cf077b7a9ef0c4c048ed724f118e64d038ff25c8
DevRaven <107134550+spowerrr@users.noreply.github.com>
2024-10-24 22:05:10 +0600

    Update lazy.lua

--- nvim/lua/josean/lazy.lua
+++ nvim/lua/josean/lazy.lua
@@ -1,16 +1,31 @@
-﻿local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- Path to lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+-- Clone lazy.nvim if not already installed
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+  if vim.fn.executable("git") == 1 then
+    local ok, result = pcall(vim.fn.system, {
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+    })
+    if not ok then
+      vim.api.nvim_err_writeln("Failed to clone lazy.nvim: " .. result)
+      return
+    end
+  else
+    vim.api.nvim_err_writeln("Git is not installed. Please install git to proceed.")
+    return
+  end
 end
+
+-- Prepend lazy.nvim to runtime path
 vim.opt.rtp:prepend(lazypath)
 
+-- Setup lazy.nvim with plugins
 require("lazy").setup({
   { import = "josean.plugins" },
   { import = "josean.plugins.lsp" },
@@ -19,9 +34,9 @@ require("lazy").setup({
   "mfussenegger/nvim-dap", -- Debug Adapter Protocol
   "rcarriga/nvim-dap-ui", -- DAP UI
   "theHamsta/nvim-dap-virtual-text", -- DAP Virtual Text
-  "tpope/vim-dispatch", -- Dispatch for running code -- for compiling and running
+  "tpope/vim-dispatch", -- Dispatch for running code
   "thinca/vim-quickrun", -- Quickrun for running code
-  "mhartington/formatter.nvim",
+  "mhartington/formatter.nvim", -- Formatter
 }, {
   checker = {
     enabled = true,
